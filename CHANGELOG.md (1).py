4af4a250eb3e4e91a793d4779fb2193f97cb7e64
njyap2030-ux <njyap2030@crc.pshs.edu.ph>
2025-08-15 22:46:08 +0800

    Add files via upload

---
+++ CHANGELOG.md (1).py
@@ -0,0 +1,17 @@
+# Changelog
+
+## [1.0.0] - 2025-08-15
+- Initial release.
+- Added feature to record expenses with date, category, and amount.
+- Added option to view all recorded expenses.
+- Added category-wise total calculation.
+
+## [1.1.0] - 2025-08-20
+- Added input validation to prevent negative or non-numeric amounts.
+- Improved menu navigation for better user experience.
+- Fixed bug where expenses were not saved properly.
+
+## [1.2.0] - 2025-08-25
+- Added feature to delete an expense from the list.
+- Added file saving and loading for expense records.
+- Enhanced category totals display with currency formatting.
---
+++ README.md (1).py
@@ -0,0 +1,51 @@
+# Expense Tracker
+
+The Expense Tracker is a simple Python program that allows users to record daily expenses, categorize them, and view total spending and category summaries.
+
+## Description
+The purpose of this project is to help users monitor their spending habits by organizing expenses into categories. It also serves as a demonstration of the programming concepts learned during the first quarter, such as variables, loops, conditionals, functions, lists, dictionaries, file handling, and error handling.
+
+## Features
+- Add a new expense with date, category, and amount.
+- View a list of all recorded expenses.
+- View total expenses for each category.
+- Delete a selected expense from the list.
+- Save and load expenses from a file for future use.
+- Input validation to prevent invalid data.
+
+## How to Run the Program
+1. Make sure you have Python installed.
+2. Download the file `grade_calculator.py`.
+3. Open a terminal or command prompt.
+4. Run the program by pressing F5 or clicking 'Run' 
+5. Follow the on-screen instructions to enter student names and grades.
+
+## Example output
+Expense Tracker
+Add Expense
+View All Expenses
+View Category Totals
+Delete Expense
+Exit
+Choose an option (1-5): 1
+Enter date (YYYY-MM-DD): 2025-08-15
+Enter category (e.g., Food, Transport, Bills): Food
+Enter amount: 250
+Expense added successfully!
+
+Expense Tracker
+Add Expense
+View All Expenses
+View Category Totals
+Delete Expense
+Exit
+Choose an option (1-5): 3
+
+Category Totals:
+Food: ₱250.00
+
+    
+## Contributors
+-Student 1: Nathan Jae L. Yap (made the README.md and did part of the code)
+-Student 2: Johannah Marie C. Serrano (made the CHANGELOG.md)
+-Student 3: Nathaniel Nino S. Tabili (made most of the code)
